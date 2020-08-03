d5860490761dbc07848e501f404758e5bb73fb1f
blacksuperdev <oyeniyi4real@gmail.com>
2020-08-04 00:53:11 +0100

    collection-item component

--- src/components/collection-preview/collection-preview.component.jsx
+++ src/components/collection-preview/collection-preview.component.jsx
@@ -3,12 +3,12 @@ import React from 'react'
 import "./collection-preview.styles.scss"
 
 
- const CollectionPreview = ({title}) => {
+ const CollectionPreview = ({title,items}) => {
     return (
         <div className="collection-preview">
             <h1>{title.toUpperCase()}</h1>
             <div className="preview">
-                {items.map(item => (
+                {items.filter((item, idx) => idx < 4).map(item => (
                     <div key={item.id}>{item.name}</div>
                 ))}
             </div>
